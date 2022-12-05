b5dcc12e5c026ac5c3152f14de61c12c30873249
CC082 <CC082@PC190131.tlgins.com.tw>
2022-12-05 11:43:16 +0800

    [20221205] 在Sim.format 增加註解

--- OtherUtil/src/main/java/com/util/hsu/Sim.java
+++ OtherUtil/src/main/java/com/util/hsu/Sim.java
@@ -3,6 +3,13 @@ package com.util.hsu;
 import java.util.Locale;
 
 public class Sim {
+	/**
+	 * 
+	 * @param l 國別
+	 * @param format 格式的字串
+	 * @param args 值
+	 * @return
+	 */
 	public static String format(Locale l, String format, Object... args) {
 		// TODO Auto-generated method stub
 		return java.lang.String.format(l, format, args);
